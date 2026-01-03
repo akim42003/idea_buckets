d86dbd8bbbaf64e7f1be84150637c2320eb29aae
akim42003 <akim42003@gmail.com>
2026-01-02 22:22:16 -0800

    Updates

---
+++ _app/immutable/chunks/BF0GOuZB.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_pn6d99?.base??"/idea_buckets",e=globalThis.__sveltekit_pn6d99?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/BX-GIDl1.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1cvcufu?.base??"/idea_buckets",e=globalThis.__sveltekit_1cvcufu?.assets??s??"";export{e as a,s as b};
