c1a3205ad065e6ce871fe587464166192a16891f
akim42003 <akim42003@gmail.com>
2026-01-02 18:40:09 -0800

    Updates

---
+++ _app/immutable/chunks/BBcxeH9g.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1oikesf?.base??"/idea_buckets",e=globalThis.__sveltekit_1oikesf?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/CKRLi0XB.js
@@ -0,0 +1,1 @@
+import{d as s,E as c,h as m,e as h}from"./ComfdDH9.js";import{B as i}from"./DKTfevBH.js";function E(n,r,e){m&&h();var o=new i(n);s(()=>{var a=r()??null;o.ensure(a,a&&(t=>e(t,a)))},c)}export{E as c};
