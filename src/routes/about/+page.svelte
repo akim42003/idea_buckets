d767970a97802290bb8915259d9f6e0bf9859a69
akim42003 <akim42003@gmail.com>
2026-01-08 23:31:25 -0500

    updated about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,11 @@
+<p>
+	About this blog; I made a similar situation for my own internal thoughts but it became difficult
+	to maintain due to some bad programming decisions and also I want to write about different things
+	in different places. So far, it's my working reading list (goodreads advertises too much) and a
+	few cobbled together essays. I'm hoping to organize this better one day, and use it as a
+	repository of thought pieces/informal writings and technical notes. For now I'm having fun writing
+	again.
+</p>
 
-
-<p> About this blog; I made a similar situation for my own internal thoughts but it became difficult to maintain due to some bad programming decisions and also I want to write about different things in different places</p>
+<p>Cheers!</p>
+<p>-Alex</p>
